6f6fabb270af02a243860961879aa5c8b0ba73c1
Domenic Datti <ddatti@outlook.com>
2016-10-26 20:17:51 -0400

    Implemented QueuedPressableInput

--- ExternalInputShared/QueuedPressableInput.cs
+++ ExternalInputShared/QueuedPressableInput.cs
@@ -1,4 +1,5 @@
 using System;
+using Apache.NMS;
 using FlatRedBall.Input;
 
 namespace ExternalInputShared
@@ -7,15 +8,60 @@ namespace ExternalInputShared
     public class QueuedPressableInput : IPressableInput, IDisposable
     {
         private readonly QueueConsumer _queueConsumer;
+        private bool _previousDown;
 
         public QueuedPressableInput()
         {
             _queueConsumer = new QueueConsumer("IPressableInput");
+
+            _queueConsumer.Listener += QueueConsumerOnListener;
         }
 
-        public bool IsDown { get; }
-        public bool WasJustPressed { get; }
-        public bool WasJustReleased { get; }
+        private void QueueConsumerOnListener(IMessage message)
+        {
+            var bytesMessage = (IBytesMessage)message;
+
+            IsDown = bytesMessage.Content[0] == 1;
+        }
+
+        public bool IsDown { get; private set; }
+        public bool WasJustPressed
+        {
+            get
+            {
+                bool retValue;
+                if (!_previousDown && IsDown)
+                {
+                    retValue = true;
+                }
+                else
+                {
+                    retValue = false;
+                }
+
+                _previousDown = IsDown;
+
+                return retValue;
+            }
+        }
+        public bool WasJustReleased {
+            get
+            {
+                bool retValue;
+                if (_previousDown && !IsDown)
+                {
+                    retValue = true;
+                }
+                else
+                {
+                    retValue = false;
+                }
+
+                _previousDown = IsDown;
+
+                return retValue;
+            }
+        }
 
         public void Dispose()
         {
